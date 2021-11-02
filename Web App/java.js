2d62cdef60b3edb9a8ea3f0bb1428224f3a177fe
Cloete <cloetedelport14@gmail.com>
2021-11-02 21:07:04 +0200

    fgj

--- Web App/java.js
+++ Web App/java.js
@@ -3,16 +3,23 @@ function newTask(){
     var newtextbox = '';
     var assignee ='';
     var date ='';
-    newtextbox += '<input type="text" id="textInput" value="Insert Task" />';
+    var deleteTask = '';
+    newtextbox += '<input placeholder="Insert Task" type="text" id="textInput"/>';
     assignee += '<button onclick="pickAssignee" value= "Assignee"> Assignee </button>';
     date += '<button onclick="pickDate" value= "Pick Date">Date</button>';
+    deleteTask += '<button onclick="delete" id="delete">Delete</button>';
     document.getElementById('tbox_div').innerHTML+=newtextbox;
     document.getElementById('tbox_div').innerHTML+=assignee;
     document.getElementById('tbox_div').innerHTML+=date;
+    document.getElementById('tbox_div').innerHTML+=deleteTask;
 }
 
 function searchStudent() {
     var newtextbox = '';
     newtextbox += '<input type="text" id ="textInput" value ="Insert Task" />';
     document.getElementById('select_div').innerHTML += newtextbox;
+}
+
+function deleteTask(){
+    var deleteMe = document.getElementById("delete");
 }
